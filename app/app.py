bddf0edeca63f7e19c3a2d2933a035fd046e77ff
Raghul <raghulm8697@gmail.com>
2026-09-21 18:41:38 +0530

    Add development environment information

--- app/app.py
+++ app/app.py
@@ -1,13 +1,14 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 VERSION = "4.2.0"
+ENVIRONMENT = "DEVELOPMENT"
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(f"Retail Platform - Version {VERSION}".encode())
+        self.wfile.write(f"Retail Platform - Version {VERSION} - {ENVIRONMENT}".encode())
 
 server = HTTPServer(("0.0.0.0", 8081), Handler)
 
